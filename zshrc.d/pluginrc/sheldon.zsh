df55e0178a3daa989a64f24a67f3d118add4dd3e
ved0el <account@le0.dev>
2024-08-09 18:36:00 +0900

    Update sheldon script

--- zshrc.d/pluginrc/sheldon.zsh
+++ zshrc.d/pluginrc/sheldon.zsh
@@ -23,18 +23,14 @@ ensure_zcompiled() {
 
 # Replace placeholders in plugins.toml with environment variable values
 plugins_toml="$SHELDON_CONFIG_DIR/plugins.toml"
-temp_plugins_toml="/tmp/plugins.toml"
 
 # Ensure proper quoting for paths and use 'sed' to replace placeholders
-sed -e "s|{{DOTFILES_DIR}}|$DOTFILES_DIR|g" "$plugins_toml" > "$temp_plugins_toml"
+sed -e "s|{{DOTFILES_DIR}}|$DOTFILES_DIR|g" "$plugins_toml" > "$plugins_toml"
 
 # Source Sheldon cache
 sheldon_cache="$SHELDON_CONFIG_DIR/cache.zsh"
-if [[ ! -r "$sheldon_cache" || "$temp_plugins_toml" -nt "$sheldon_cache" ]]; then
+if [[ ! -r "$sheldon_cache" || "$plugins_toml" -nt "$sheldon_cache" ]]; then
   sheldon source > "$sheldon_cache"
 fi
 source "$sheldon_cache"
 unset sheldon_cache
-
-# Clean up temporary file
-rm "$temp_plugins_toml"
