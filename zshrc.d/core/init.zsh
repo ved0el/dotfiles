9045c96683c1e6fa9b151d982ec3aaa40a46d94f
leo@htj <lequanghung@duck.com>
2025-04-08 16:43:10 +0900

    update: turn p10k instant promt off

--- zshrc.d/core/init.zsh
+++ zshrc.d/core/init.zsh
@@ -1,4 +1,6 @@
-# Enable Powerlevel10k instant prompt
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
+# # Enable Powerlevel10k instant prompt
+# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+# fi
+
+typeset -g POWERLEVEL9K_INSTANT_PROMPT=off
