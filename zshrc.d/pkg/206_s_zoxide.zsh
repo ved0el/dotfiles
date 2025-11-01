148c0d2fe4c2ccb91144f51a07bc1cdfd55305a1
Leo@HTJ <lequanghung@duck.com>
2025-11-01 17:03:08 +0700

    update: zoxide options

--- zshrc.d/pkg/206_s_zoxide.zsh
+++ zshrc.d/pkg/206_s_zoxide.zsh
@@ -7,6 +7,7 @@ pkg_init() {
     eval "$(zoxide init zsh)"
     alias cd="z"
     alias cdi="zi"
+    export _ZO_FZF_OPTS="--preview 'eza -al --tree --level 1 --group-directories-first --header --no-user --no-time --no-filesize --no-permissions {2..}' --preview-window right,50% --height 35% --reverse --ansi --with-nth 2.."
 }
 
 init_package_template "$PKG_NAME"
