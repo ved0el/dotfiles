6b16e69c86af5d95393d2b92874bcb894ad184ed
Leo@HTJ <lequanghung@duck.com>
2025-12-23 16:06:30 +0900

    update: eza command with group

--- zshrc.d/pkg/202_s_eza.zsh
+++ zshrc.d/pkg/202_s_eza.zsh
@@ -4,11 +4,11 @@ PKG_NAME="eza"
 PKG_DESC="A modern replacement for ls"
 
 pkg_init() {
-  alias ls="eza"
-  alias ll="eza -l"
-  alias la="eza -la"
+  alias ls="eza -g"
+  alias ll="eza -lg"
+  alias la="eza -lag"
   alias lt="eza --tree"
-  alias lta="eza --tree -a"
+  alias lta="eza --tree -ag"
 }
 
 init_package_template "$PKG_NAME"
