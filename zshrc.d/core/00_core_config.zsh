0f98b6d0fe96ac70776b4b4806490cb90cb70992
Leo <lequanghung@duck.com>
2026-01-03 22:00:50 +0900

    update: default terminal config

--- zshrc.d/core/00_core_config.zsh
+++ zshrc.d/core/00_core_config.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+export TERM="xterm-256color"
+
 # Essential environment variables and PATH setup
 [[ ":$PATH:" != *":$DOTFILES_ROOT/bin:"* ]] && export PATH="$PATH:$DOTFILES_ROOT/bin"
 [[ ":$PATH:" != *":$HOME/.local/bin:"* ]] && export PATH="$PATH:$HOME/.local/bin"
